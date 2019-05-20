01eb36058928372bd32c5938a18f1358591901ab
Shyim <6224096+shyim@users.noreply.github.com>
2019-05-20 19:24:27 +0200

    Create demo.sql

---
+++ src/Resources/demo.sql
@@ -0,0 +1,2 @@
+INSERT INTO system_config (id, configuration_key, configuration_value, created_at) VALUES(UNHEX('6f84227cd4cc4ccda336e94fa038336b'), 'Performance.config.enableMinify', '{"_value":true}', NOW());
+INSERT INTO system_config (id, configuration_key, configuration_value, created_at) VALUES(UNHEX('a36f48c7d8d3-4cbcad8d361211fb0c79'), 'Performance.config.http2Push', '{"_value":true}', NOW());
